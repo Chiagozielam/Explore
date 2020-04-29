f30a59d9330cc38780b6e8059ac666fd25a8e16b
Daniel Don <dandons25@gmail.com>
2020-04-29 18:39:16 +0100

    finally getting this initial zoom in thing to stop

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,46 +8,92 @@ const Home = () => {
   const { Column } = Grid;
   return (
     <div>
-      <Grid>
-        <Responsive minWidth={760}>
-          <Column width={4}>
-            <Advertisement unit="half page" test="Half Page" />
-          </Column>
-        </Responsive>
-        <Column mobile={16} tablet={12} computer={12}>
-          <h2 className="explore-header">Explore</h2>
-          <SearchBar />
-          <p className="home-top-cards-headline"><span>Discover</span> new places</p>
-          <Grid className="top-card-grid space">
-            <Column mobile={12} tablet={5} computer={5}>
+
+
+      <div className="mobile-topo-cards-container">
+        <h2 className="explore-header">Explore</h2>
+        <SearchBar />
+        <p className="home-top-cards-headline"><span>Discover</span> new places</p>
+        <Grid className="top-card-grid space">
+            <Column mobile={12}>
               <TopCard 
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992037/ajt-bin-haddu-4753097_640.jpg"
                 continent="Africa"
                 description="View all amazing and stunning places around Africa"
               />
             </Column>
-            <Column mobile={12} tablet={5} computer={5}>
+            <Column mobile={12}>
               <TopCard 
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1588036752/statue-of-liberty-271430_640.jpg"
                 continent="America"
                 description="View all amazing and stunning places around America"
               />
             </Column>
-            <Column mobile={12} tablet={5} computer={5}>
+            <Column mobile={12}>
               <TopCard 
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992036/sydney-4034244_640.jpg"
                 continent="Australia"
                 description="View all amazing and stunning places around Australia"
               />
             </Column>
-            <Column mobile={12} tablet={5} computer={5}>
+            <Column mobile={12}>
               <TopCard 
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992037/takshang-669959_640.jpg"
                 continent="Asia"
                 description="View all amazing and stunning places around Asia"
               />
             </Column>
-            <Column mobile={12} tablet={5} computer={5}>
+            <Column mobile={12}>
+              <TopCard
+                imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992036/river-690374_640.jpg"
+                continent="Europe"
+                description="View all amazing and stunning places around Europe"
+              />
+            </Column>
+          </Grid>
+      </div>
+
+
+      <Grid>
+        <Responsive minWidth={760}>
+          <Column width={4}>
+            <Advertisement unit="half page" test="Half Page" />
+          </Column>
+        </Responsive>
+        <Column mobile={16} tablet={12} computer={12} className="top-cards-container">
+          <h2 className="explore-header">Explore</h2>
+          <SearchBar />
+          <p className="home-top-cards-headline"><span>Discover</span> new places</p>
+          <Grid className="top-card-grid space">
+            <Column mobile={12}>
+              <TopCard 
+                imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992037/ajt-bin-haddu-4753097_640.jpg"
+                continent="Africa"
+                description="View all amazing and stunning places around Africa"
+              />
+            </Column>
+            <Column mobile={12}>
+              <TopCard 
+                imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1588036752/statue-of-liberty-271430_640.jpg"
+                continent="America"
+                description="View all amazing and stunning places around America"
+              />
+            </Column>
+            <Column mobile={12}>
+              <TopCard 
+                imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992036/sydney-4034244_640.jpg"
+                continent="Australia"
+                description="View all amazing and stunning places around Australia"
+              />
+            </Column>
+            <Column mobile={12}>
+              <TopCard 
+                imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992037/takshang-669959_640.jpg"
+                continent="Asia"
+                description="View all amazing and stunning places around Asia"
+              />
+            </Column>
+            <Column mobile={12}>
               <TopCard
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992036/river-690374_640.jpg"
                 continent="Europe"
