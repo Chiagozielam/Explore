6f4c11fa5e28ac173b222b9d0b0dc934343b9670
Daniel Don <dandons25@gmail.com>
2020-04-29 03:09:11 +0100

    vamped mobile view and scattered desktop view in the process. There is a cost for everything in lifegit add .!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import { Responsive } from 'semantic-ui-react';
 import TopNav from './components/navigation/TopNav';
 import WelcomePage from './pages/welcome';
 import HomePage from './pages/home';
@@ -8,7 +9,9 @@ import HomePage from './pages/home';
 function App() {
   return (
     <div className="App">
-      <TopNav />
+      <Responsive minWidth={760}>
+        <TopNav />
+      </Responsive>
       <div className="full-app-wrap">
         <Switch>
           <Route exact path="/" component={WelcomePage} />
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,50 +1,52 @@
 import React from 'react';
-import { Grid, Advertisement } from 'semantic-ui-react'
+import { Grid, Advertisement, Responsive } from 'semantic-ui-react'
 import SearchBar from '../components/search/SearchBar';
 import { TopCard, RegularCard } from '../components/cards'
-import './css/home.css'
+import './css/home.css';
 
 const Home = () => {
   const { Column } = Grid;
   return (
     <div>
       <Grid>
-        <Column width={4}>
-          <Advertisement unit="half page" test="Half Page" />
-        </Column>
-        <Column width={12}>
+        <Responsive minWidth={760}>
+          <Column width={4}>
+            <Advertisement unit="half page" test="Half Page" />
+          </Column>
+        </Responsive>
+        <Column mobile={16} width={12}>
           <SearchBar />
           <p className="space home-top-cards-headline"><span>Discover</span> new places</p>
           <Grid className="top-card-grid">
-            <Column width={5}>
+            <Column mobile={12} tablet={5} computer={5}>
               <TopCard 
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992037/ajt-bin-haddu-4753097_640.jpg"
                 continent="Africa"
                 description="View all amazing and stunning places around Africa"
               />
             </Column>
-            <Column width={5}>
+            <Column mobile={12} width={5}>
               <TopCard 
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1588036752/statue-of-liberty-271430_640.jpg"
                 continent="America"
                 description="View all amazing and stunning places around America"
               />
             </Column>
-            <Column width={5}>
+            <Column mobile={12} width={5}>
               <TopCard 
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992036/sydney-4034244_640.jpg"
                 continent="Australia"
                 description="View all amazing and stunning places around Australia"
               />
             </Column>
-            <Column width={5}>
+            <Column mobile={12} width={5}>
               <TopCard 
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992037/takshang-669959_640.jpg"
                 continent="Asia"
                 description="View all amazing and stunning places around Asia"
               />
             </Column>
-            <Column width={5}>
+            <Column mobile={12} width={5}>
               <TopCard
                 imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992036/river-690374_640.jpg"
                 continent="Europe"
@@ -57,30 +59,25 @@ const Home = () => {
       <section className="space top-locations-section">
         <p className="space home-top-cards-headline"><span>Most</span> visited places</p>
           <Grid>
-            <Column width={4}>
+            <Column mobile={8} tablet={4} computer={4}>
               <RegularCard />
             </Column>
-            <Column width={4}>
+            <Column mobile={8} tablet={4} computer={4}>
               <RegularCard />
             </Column>
-            <Column width={4}>
+            <Column mobile={8} tablet={4} computer={4}>
               <RegularCard />
             </Column>
-            <Column width={4}>
+            <Column mobile={8} tablet={4} computer={4}>
               <RegularCard />
             </Column>
-            <Column width={4}>
+            <Column mobile={8} tablet={4} computer={4}>
               <RegularCard />
             </Column>
-            <Column width={4}>
+            <Column mobile={8} tablet={4} computer={4}>
               <RegularCard />
             </Column>
-            <Column width={4}>
-              <RegularCard />
-            </Column>
-            <Column width={4}>
-              <RegularCard />
-            </Column>
+            
           </Grid>
           </section>
           <div className="space" style={{height: "1000px"}}></div>
