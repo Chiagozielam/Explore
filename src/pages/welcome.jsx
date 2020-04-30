318bbebd6bc0104c0a551ecc824c6db92e3f1aed
Daniel Don <dandons25@gmail.com>
2020-04-30 16:19:38 +0100

    redesign landing page to a simple one-page design

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -1,94 +1,27 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-// Import slides from "react-swipable-views"
-import Button from "@material-ui/core/Button";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { red, blue, green } from "@material-ui/core/colors";
-import { AutoRotatingCarousel, Slide } from "material-auto-rotating-carousel";
+import React from "react";
+import { Link } from "react-router-dom"
+import "./css/welcome.css";
 
-const Welcome = (props) => {
-  const history = useHistory();
-
-  const [handleOpen, setHandleOpen] = useState({ open: true });
-  // const handleClick = () => {
-  //   setHandleOpen({ open: true });
-  // };
-  const matches = useMediaQuery("(max-width:600px)");
-
-  const onCloseFunc = () => {
-    setHandleOpen({ open: false });
-    history.push("/home");
-  };
-
+const Landing = () => {
   return (
     <div>
-      {/* <Button onClick={() => setHandleOpen({ open: true })}>Open carousel</Button> */}
-      <AutoRotatingCarousel
-        label="Get started"
-        open={handleOpen.open}
-        onClose={onCloseFunc}
-        onStart={onCloseFunc}
-        autoplay={false}
-        mobile={matches}
-        style={{ position: "absolute" }}
-      >
-        <Slide
-          media={
-            <img
-              src="https://res.cloudinary.com/dcft8yhab/image/upload/v1587463594/image_2.png"
-              alt=""
-            />
-          }
-          mediaBackgroundStyle={{ backgroundColor: '#FF4500' }}
-          style={{ backgroundColor: '#FF4500' }}
-          title="EXPLORE"
-          subtitle="Let’s bring you tourism from around the world"
+      <div className="landing-img">
+        <img
+          src="https://res.cloudinary.com/dcft8yhab/image/upload/v1588256232/explore.png"
+          alt=""
         />
-        <Slide
-          media={
-            <img
-              src="https://res.cloudinary.com/dcft8yhab/image/upload/v1587463594/image_2.png"
-              alt=""
-            />
-          }
-          mediaBackgroundStyle={{ backgroundColor: '#FF4500' }}
-          style={{ backgroundColor: '#FF4500' }}
-          title="EXPLORE"
-          subtitle="Anywhere country or location you want to visit. We provide you with super tourism locations around there in seconds."
-        />
-        <Slide
-          media={
-            <img
-              src="https://res.cloudinary.com/dcft8yhab/image/upload/v1587463588/undraw_game_world_0o6q_1.png"
-              alt=""
-            />
-          }
-          mediaBackgroundStyle={{ backgroundColor: '#FF4500' }}
-          style={{ backgroundColor: '#FF4500' }}
-          title="EXPLORE"
-          subtitle="To cut the long story short: Just travel around the world while we help you find where next to go"
-        />
-      </AutoRotatingCarousel>
+      </div>
+      <p className="landing-text">
+        View amazing tourism locations and interesting facts about places
+        from around the world
+      </p>
+      <div>
+        <Link to="/home"><button className="landing-btn">View locations</button></Link>
+        <Link to="/home"><button className="landing-btn-mobile">View locations</button></Link>
+
+      </div>
     </div>
   );
 };
 
-const Test = () => {
-  const [handleOpen, setHandleOpen] = useState({ open: false });
-  const handleClick = () => {
-    setHandleOpen({ open: true });
-  };
-  const matches = useMediaQuery("(max-width:600px)");
-  return (
-    <>
-      <Button onClick={handleClick}>Open carousel</Button>
-      <Welcome
-        isMobile={matches}
-        handleOpen={handleOpen}
-        setHandleOpen={setHandleOpen}
-      />
-    </>
-  );
-};
-
-export default Welcome;
+export default Landing;
