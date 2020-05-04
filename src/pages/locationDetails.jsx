1fdfc77272c4e0f82b5c189746499f931a0554fb
Daniel Don <dandons25@gmail.com>
2020-05-04 23:49:42 +0100

    make some changes

--- src/pages/locationDetails.jsx
+++ src/pages/locationDetails.jsx
@@ -8,42 +8,42 @@ const locationDetails = () => {
     <div>
       <Grid>
         <Row>
-          <Responsive maxWidth={760}>
+          <Column mobile={16} width={7}>
+            <div className="location-details">
+              <h1 className="location-name">Morocco, Sanctatori</h1>
+              <p className="location-details-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation
+              </p>
+              <button className="location-share-btns facebook-blue">share on facebook</button>
+              <button className="location-share-btns twitter-blue">share on twitter</button>
+              <div className="location-extras">
+                <Grid>
+                  <Column width={4}>
+                    <button class="location-extras-boxes dark-theme">book a <br/> flight</button>
+                  </Column>
+                  <Column width={4}>
+                    <button class="location-extras-boxes">hotel<br/> reservation</button>
+                  </Column>
+                  <Column width={4}>
+                    <button class="location-extras-boxes dark-theme">request a <br/> travel guide</button>
+                  </Column>
+                </Grid>
+              </div>
+            </div>
+          </Column>
+          <Responsive minWidth={760}>
             <Column width={7}>
-              <div className="location-details">
-                <h1 className="location-name">Morocco, Sanctatori</h1>
-                <p className="location-details-text">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                  enim ad minim veniam, quis nostrud exercitation
-                </p>
-                <button className="location-share-btns facebook-blue">share on facebook</button>
-                <button className="location-share-btns twitter-blue">share on twitter</button>
-                <div className="location-extras">
-                  <Grid>
-                    <Column width={4}>
-                      <button class="location-extras-boxes dark-theme">book a <br/> flight</button>
-                    </Column>
-                    <Column width={4}>
-                      <button class="location-extras-boxes">hotel<br/> reservation</button>
-                    </Column>
-                    <Column width={4}>
-                      <button class="location-extras-boxes dark-theme">request a <br/> travel guide</button>
-                    </Column>
-                  </Grid>
-                </div>
+              <div className="location-img-container-box">
+                <img
+                  src="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992037/ajt-bin-haddu-4753097_640.jpg"
+                  alt=""
+                  className="location-details-img"
+                />
               </div>
             </Column>
           </Responsive>
-          <Column mobile={14} width={7}>
-            <div className="location-img-container-box">
-              <img
-                src="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992037/ajt-bin-haddu-4753097_640.jpg"
-                alt=""
-                className="location-details-img"
-              />
-            </div>
-          </Column>
         </Row>
       </Grid>
     </div>
