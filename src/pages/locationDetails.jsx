10117d9933caf495d5b399e27a8889b4b102f867
Daniel Don <dandons25@gmail.com>
2020-05-07 03:30:36 +0100

    add location details page and modify a little few more things

--- src/pages/locationDetails.jsx
+++ src/pages/locationDetails.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./css/locationDetails.css";
-import { Grid, Responsive } from "semantic-ui-react";
+import { Grid, Responsive, Icon } from "semantic-ui-react";
 import LocationDetailCard from "../components/cards/LocationDetailCard";
+import { Link } from "react-router-dom"
 
 const locationDetails = () => {
   const { Column, Row } = Grid;
@@ -60,17 +61,46 @@ const locationDetails = () => {
       </Responsive>
 
       <Responsive maxWidth={760}>
+        <div className="back-direction-box">
+          <Link to="/home"><Icon name="arrow left" className="back-arrow" /></Link>
+        </div>
         <LocationDetailCard
           imageUrl="https://res.cloudinary.com/dcft8yhab/image/upload/v1587992037/ajt-bin-haddu-4753097_640.jpg"
           location="Morocco Tourism Bricks"
         />
-        <p>
-          thi si ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-          thi si ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-          thi si ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-          thi si ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-          thi si ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-        </p>
+        <div className="location-detail-rest-of-body-container">
+          <p>
+            In the event of a validation error when creating or updating an
+            object, response format is slightly different and contains an error
+            key instead of a data key. This example is a response you will get.
+          </p>
+          <button className="location-share-btns facebook-blue">
+            share on facebook
+          </button>
+          <button className="location-share-btns twitter-blue">
+            share on twitter
+          </button>
+
+          <div className="location-extras">
+            <Grid>
+              <Column width={5}>
+                <button class="location-extras-boxes dark-theme">
+                  flight booking
+                </button>
+              </Column>
+              <Column width={5}>
+                <button class="location-extras-boxes">
+                  hotel reservation
+                </button>
+              </Column>
+              <Column width={5}>
+                <button class="location-extras-boxes dark-theme">
+                  travel guide
+                </button>
+              </Column>
+            </Grid>
+          </div>
+        </div>
       </Responsive>
     </div>
   );
